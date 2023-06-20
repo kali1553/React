ed25aef440d33e961d144eae2164c123087940aa
kali1553 <pedrinhodeliberal@gmail.com>
2023-06-20 20:17:40 -0300

    atualizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react'
 import Banner from './components/banner'
 import Formulario from './components/Formulário'
+import Game from './components/game'
 
 export default function App() {
   
+  const [novoGame, setNovo] = useState([])
+  
+  const aoNovoGame = (Game) => {
+    console.log(Game)
+  }
+
   return (
     <div>
       <Banner/>
-      <Formulario/>
+      <Formulario aoNovoGame={Game => aoNovoGame(Game)}/>
+      <Game nome='Ação' />
     </div>
     
   )
---
+++ src/components/game/index.jsx
@@ -0,0 +1,12 @@
+import './game.css'
+
+
+const Game = (props) => {
+    return( 
+        <section className='game'>
+            <h3>{props.nome}</h3>
+        </section>
+    )
+}
+
+export default Game
